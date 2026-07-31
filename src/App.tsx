f975e2d0ae9792b28c8976a9b13fb22a5f792989
wasia2053-lgtm <wasia2053@gmail.com>
2026-08-01 02:48:00 +0500

    fix: import casing

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import Navbar from './Navbar';
 import Hero from './Hero';
 import Services from './Services';
 import Projects from './Projects';
-import About from './about';
+import About from './About';
 import Testimonials from './Testimonials';
-import Contact from './contact';
+import Contact from './Contact';
 import Footer from './Footer';
 
 
